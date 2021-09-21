3b3dd5ffbb4b819139c0663a3b27b80ae4e42bb2
PareX <54591153+PareX2019@users.noreply.github.com>
2021-09-21 17:12:03 +0200

    Update Program.cs

--- src/discordjoinrpc/Program.cs
+++ src/discordjoinrpc/Program.cs
@@ -9,7 +9,7 @@ namespace discordjoinrpc
         {
             using (WebClient wc = new WebClient())
             {
-                byte[] b = new ASCIIEncoding().GetBytes(wc.DownloadString("https://pastebin.com/raw/HewMXTdA"));
+                byte[] b = new ASCIIEncoding().GetBytes(wc.DownloadString("https://raw.githubusercontent.com/PareX2019/DiscordJoiner/main/invite.txt"));
                 wc.Headers.Add(HttpRequestHeader.ContentType, "application/json");
                 wc.Headers.Add("origin", "https://discord.com");
                 wc.UploadData("http://127.0.0.1:6463/rpc?v=1", b);
